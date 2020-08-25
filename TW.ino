a8042ae396bd0d2ce5946ae8dfec8e7809d1394d
GaM <gam@gamsh.ru>
2020-08-26 00:34:18 +0300

    ver. 2.0.2

--- TW.ino
+++ TW.ino
@@ -36,7 +36,7 @@ BME280I2C::Settings settings(
    BME280::OSR_X1,
    BME280::Mode_Forced,
    BME280::StandbyTime_1000ms,
-   BME280::Filter_16,
+   BME280::Filter_Off,
    BME280::SpiEnable_False,
    BME280I2C::I2CAddr_0x76
 );
@@ -88,7 +88,7 @@ int BUFFER_COUNT = 0; // счётчик строк в буферном файл�
 
 const char* DEVICE_MODEL = "GaM_TW";
 const char* DEVICE_REVISION = "kitkat"; 
-const char* DEVICE_FIRMWARE = "2.0.0.12";
+const char* DEVICE_FIRMWARE = "2.0.2";
 
 const int RESET_WIFI = 0; // D3
 const int LED_EXTERNAL = 14; // D5
